4de861a53b424bd494d4718466711701ad6668dc
Swarnil Kokulwar <swarnil.sk@gmail.com>
2025-05-15 22:42:57 +0530

    promise practice

---
+++ JS/practice-1/practice.js
@@ -0,0 +1,55 @@
+// Create two variables, firstName and lastName
+let firstName = Daddy
+let lastName = Joker
+
+// Concatenate the two variables into a third variable called fullName
+let third = firstName + " " +  lastName
+
+// Log fullName to the console
+console.log(third);
+
+
+
+// Create a function that logs out "Hi there, Linda!" when called
+
+let name = "Linda"
+let greeting = "Hi there"
+
+
+function greetLinda() {
+    console.log(greeting + ", " + name + "!")
+}
+
+greetLinda()
+
+
+
+// Create two functions, add3Points() and remove1Point(), and have them
+let myPoints = 3
+// add/remove points to/from the myPoints variable
+
+function add3Points() {
+    myPoints += 3
+}
+
+function remove1Points() {
+    myPoints -= 1
+}
+
+add3Points()
+add3Points()
+add3Points()
+remove1Points()
+remove1Points()
+
+// Call the functions to that the line below logs out 10
+console.log(myPoints)
+
+
+// Try to predict what each of the lines will log out
+console.log("2" + 2) // "22" 
+console.log(11 + 7) //  18
+console.log(6 + "5") // "65"
+console.log("My points: " + 5 + 9) // "My points: 59"
+console.log(2 + 2) // 4
+console.log("11" + "14") //  "1114"
